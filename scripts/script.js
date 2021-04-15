48462f66e0b565b38150ee415d41e64ad7001aa8
M-ar-sh-h <pkdash1947.er@gmail.com>
2021-04-15 11:47:13 +0530

    Initial commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("nav").style.top = "-62px";
+  } else {
+    document.getElementById("nav").style.top = "0";
+  }
+  prevScrollpos = currentScrollPos;
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+// $('.carousel').carousel({
+// 	interval: false
+// });
+
+// window.onscroll = function() {myFunction()};
+
+// function myFunction() {
+//   var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+//   var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+//   var scrolled = (winScroll / height) * 100;
+//   document.getElementById("myBar").style.width = scrolled + "%";
+// }
